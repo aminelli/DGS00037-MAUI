139ec4b0eb666936de743be1cf4cc8862eb31455
aminelli <antonio.minelli@etlforma.com>
2026-01-13 12:57:44 +0100

    upd

---
+++ EXAMPLES/APP003/CustomLayout/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomLayout {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+        }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ EXAMPLES/APP003/CustomLayout/AppShell.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace CustomLayout {
+    public partial class AppShell : Shell {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
