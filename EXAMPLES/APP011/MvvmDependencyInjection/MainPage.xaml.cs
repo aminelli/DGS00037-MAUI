b6e14b66e5def6833c96e096ff004a7323967742
aminelli <antonio.minelli@etlforma.com>
2026-01-14 09:49:35 +0100

    upd

---
+++ EXAMPLES/APP011/MvvmDependencyInjection/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace DecoupleViewAndViewModel {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+    }
+}
